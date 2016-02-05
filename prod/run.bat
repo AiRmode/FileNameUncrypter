b9ce3f3a9eb8f5eb63f1644b32fd82d7434fca79
Alexey Shevchuk <ashevchuk@selectica.com>
2016-02-05 11:43:47 +0200

    Package zip

--- prod/run.bat
+++ prod/run.bat
@@ -3,9 +3,10 @@
 :email-iizomer@aol.com.ver-CL 1.2.0.0.id-JKKLMMNNOOOPPQQRRSSSSTTUUVVWXXXXYYZZ-09.11.2015 9@53@161619071.randomname-DFFGHIIJJJKKLMMNNOOOOPPQQRRSSS.TTU.cbf
 
 :attributes:
+
 :-virVersion - like "CL 1.2.0.0" from example above
 :-fileExtension - file extension, in my example - .cbf
-:-emailPrefix - "email-iizomer@aol.com" (exactly with "email-")
+ :-emailPrefix - "email-iizomer@aol.com" (exactly with "email-")
 :-maxFileSize - file larger then specified value in bytes will not be scaned. file size depends on available RAM at your machine. Feel free to change -Xmx option if needed for big files
 java -Xmx768m -jar FileNameUncrypter.jar -virVersion "CL 1.2.0.0" -fileExtension .cbf -emailPrefix email-iizomer@aol.com -maxFileSize 99999999
 PAUSE
