c66fd5c48022b8bf9b6086f4a9d407e396c4a222
Alexey Shevchuk <ashevchuk@selectica.com>
2016-02-11 09:52:19 +0200

    Report sorted by file path desc

--- src/p1/FinePrinter.java
+++ src/p1/FinePrinter.java
@@ -1,6 +1,7 @@
 package p1;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by alshevchuk on 30.01.2016.
@@ -8,6 +9,7 @@ import java.util.Map;
 public class FinePrinter {
 
     public static String createHTMLLikeFile(Map<String, String> map) {
+        map = new TreeMap<>(map);
         StringBuffer sb = new StringBuffer("<meta http-equiv=\"Content-Type\" content=\"text/html;charset=Windows-1251\">");
         for (String key : map.keySet()) {
             sb.append(key).append("</br><b>").append(map.get(key)).append("</b></br></br>");
